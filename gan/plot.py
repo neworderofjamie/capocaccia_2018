7d93ef9c9b69f8289d22782ccf2c98cc7ded6e50
jamie <neworderofjamie@gmail.com>
2018-05-01 15:33:56 +0100

    Tweaks * New AdExp model with external current state variable * Correct AdExp parameters * Attempt at useful stimulation * Recording of spikes and analogue variables * Plotting script

--- gan/plot.py
+++ gan/plot.py
@@ -2,26 +2,44 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("data.csv", "rb") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
+with open("spikes.csv", "rb") as spike_csv_file, open("voltages.csv", "rb") as voltage_csv_file:
+    spike_csv_reader = csv.reader(spike_csv_file, delimiter = ",")
+    voltage_csv_reader = csv.reader(voltage_csv_file, delimiter = ",")
 
+    # Skip headers
+    spike_csv_reader.next()
+    voltage_csv_reader.next()
+    
     # Read data and zip into columns
-    data_columns = zip(*csv_reader)
+    spike_data_columns = zip(*spike_csv_reader)
+    voltage_data_columns = zip(*voltage_csv_reader)
 
-    # Convert times to numpy
-    times = np.asarray(data_columns[0], dtype=float)
-    voltage = np.asarray(data_columns[1], dtype=float)
-    adaption = np.asarray(data_columns[2], dtype=float)
-
+    # Convert to numpy
+    spike_times = np.asarray(spike_data_columns[0], dtype=float)
+    spike_id = np.asarray(spike_data_columns[1], dtype=int)
+    voltage_time = np.asarray(voltage_data_columns[0], dtype=float)
+    voltage = np.asarray(voltage_data_columns[2], dtype=float)
+    
+    # Reshape voltage so it's one column per neuron
+    voltage_time = voltage_time.reshape((-1, 4))
+    voltage = voltage.reshape((-1, 4))
+    
     # Create plot
-    figure, axes = plt.subplots(2)
+    figure, axes = plt.subplots(2, sharex=True)
 
     # Plot voltages
-    axes[0].set_title("Voltage")
-    axes[0].plot(times, voltage)
+    for i in range(voltage.shape[1]):
+        axes[0].plot(voltage_time[:,i], voltage[:,i], label="%u" % i)
     
-    axes[1].set_title("Adaption")
-    axes[1].plot(times, adaption)
-
+    # Plot spikes
+    axes[1].scatter(spike_times, spike_id, s=2)
+    
+    axes[1].set_ylim((0, 4))
+    axes[0].set_ylabel("Membrane voltage [mV]")
+    axes[1].set_xlabel("Time [ms]")
+    axes[1].set_ylabel("Neuron ID")
+    
+    axes[0].legend()
+    
     # Show plot
     plt.show()
--- gan/simulator.cc
+++ gan/simulator.cc
@@ -1,4 +1,5 @@
 // GeNN robotics includes
+#include "analogue_csv_recorder.h"
 #include "spike_csv_recorder.h"
 
 // Auto-generated model code
@@ -18,10 +19,23 @@ void setInhibitoryWeight(unsigned int preIdx, unsigned int postIdx, scalar weigh
 {
     gInhibitorySyn[(preIdx * 4) + postIdx] = weight;
 }
+
+void setBlueInput(scalar value)
+{
+    iExtNeurons[0] = -7.13f * value * 0.003f * 100000.0f;
+}
+
+void setRedInput(scalar value)
+{
+    iExtNeurons[1] = 4.58f * value * 0.003f * 100000.0f;
+}
 }   // Anonymous namespace
 
 int main()
 {
+    const scalar leftValue = 0.55f;
+    const scalar rightValue = 0.45f;
+    
     allocateMem();
     initialize();
   
@@ -38,21 +52,30 @@ int main()
   
     // Open CSV output files
     SpikeCSVRecorder spikes("spikes.csv", glbSpkCntNeurons, glbSpkNeurons);
+    AnalogueCSVRecorder<scalar> voltages("voltages.csv", VNeurons, 4, "Membrane voltage [mV]");
 
     // Loop through timesteps
-    for(unsigned int i = 0; i < 10000; i++)
-    {
+    while(t < 200.0f) {
+        // Apply some stimuli
+        if(t > 10.0f && t < 60.0f) {
+            setRedInput(leftValue);
+            setBlueInput(0.0f);
+        }
+        else if(t > 70.0f && t < 120.0f) {
+            setRedInput(0.0f);
+            setBlueInput(leftValue);
+        }
+        else {
+            setRedInput(0.0f);
+            setBlueInput(0.0f);
+        }
+        
         // Simulate
-#ifndef CPU_ONLY
-        stepTimeGPU();
-
-        pullECurrentSpikesFromDevice();
-        //pullIEStateFromDevice();
-#else
         stepTimeCPU();
-#endif
 
+        // Record spikes and voltage
         spikes.record(t);
+        voltages.record(t);
 
     }
     
